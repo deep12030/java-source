aa9e3bfea1b5710d7fe1290fe6131735f573a9d3
kumar <kumar@LAPTOP-39HB4SPG>
2020-01-25 14:53:14 +0100

    first commit

---
+++ test/src/test/testing.java
@@ -0,0 +1,11 @@
+package test;
+
+public class testing {
+	{
+		System.out.println("dipaly() in p1");
+	}
+    public static void main(String args[]){	
+    System.out.println("Hello package p1");
+    }
+
+}
